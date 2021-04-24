c72a278b0b492796bb44bd8e191e6ee1454d8044
SumitNagpall <82007132+SumitNagpall@users.noreply.github.com>
2021-04-24 11:41:26 +0530

    Updated ArrayMissingDuplicateKSum.java
    Finding Missing Values in Sorted Array
    Finding Duplicate Values without extra Array O(n2)
    Finding Duplicate Values with extra Array O(n)
    Finding duplicates in Sorted Array 
    Finding duplicates in UnSorted Array
    Finding duplicates in UnSorted Array using extra Array O(n)
    Finding  sum K in Unsorted Array O(n2)
    Finding  sum K in Unsorted Array using extra Array O(n)
    Finding  sum K in Sorted Array O(n)

--- ArrayMissingDuplicateKSum.java
+++ ArrayMissingDuplicateKSum.java
@@ -93,6 +93,40 @@ class ArrayMissingDuplicateKSum
 			if(array[i]>1)
 				System.out.println(i+" is present "+array[i]+" times.");	
 	}
+	public static void sumKUnsorted(int[] arr,int k)
+	{
+		for(int i=0;i<arr.length;i++)
+			for(int j=i+1;j<arr.length;j++)
+				if(arr[i]+arr[j]==k)
+					System.out.print("("+arr[i]+" , "+arr[j]+")  ");
+	}
+	public static void sumKUnsortedArray(int[] arr,int k)
+	{
+		int max=arr[0];
+		for(int i=1;i<arr.length;i++)
+			if(arr[i]>max)
+				max=arr[i];
+		int array[]=new int[max+1];
+		for(int i=0;i<arr.length;i++)
+		{
+			if(k-arr[i]>=0&&k-arr[i]<=max&&array[k-arr[i]]>0)
+				System.out.print("("+arr[i]+" , "+(k-arr[i])+")  ");
+			array[arr[i]]++;
+		}
+	}
+	public static void sumKSorted(int[] arr,int k)
+	{
+		int i=0,j=arr.length-1;
+		while(i<j)
+		{
+			if(arr[i]+arr[j]==k)
+				System.out.print("("+arr[i++]+" , "+arr[j--]+")  ");
+			else if(arr[i]+arr[j]>k)
+				j--;
+			else
+				i++;
+		}
+	} 
 	public static void main(String[] args)
 	{
 		int arr[]={1,2,3,6};
@@ -105,5 +139,10 @@ class ArrayMissingDuplicateKSum
 		ArrayMissingDuplicateKSum.duplicatesUnSorted(arr2);
 		int arr3[]={1,4,2,8,4,5,7,2,7,1,3,2};
 		ArrayMissingDuplicateKSum.duplicatesUnSortedArray(arr3);
+		int arr4[]={3,4,6,2,8,1,2};
+		ArrayMissingDuplicateKSum.sumKUnsorted(arr4,10);
+		ArrayMissingDuplicateKSum.sumKUnsortedArray(arr4,10);
+		int arr5[]={1,2,4,6,8,9};
+		ArrayMissingDuplicateKSum.sumKSorted(arr5,10);
 	}
 }
